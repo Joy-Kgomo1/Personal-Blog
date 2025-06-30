becc99a7580bb7b2d5ada0613f8f32b479dcdcef
Joy-Kgomo1 <joykgomo11@gmail.com>
2025-06-30 22:34:14 +0200

    Completed the AdminController

--- Personal Blog/Controllers/AdminController.cs
+++ Personal Blog/Controllers/AdminController.cs
@@ -1,12 +1,81 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Personal_Blog.Models;
+using Personal_Blog.Services;
 
 namespace Personal_Blog.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private readonly ArticleService _service;
+        public AdminController(ArticleService service)
+        {
+            _service = service;
+            
+        }
+        public IActionResult Dashboard()
+        {
+            var articles = _service.GetArticles();
+            return View(articles);
+        }
+
+        public IActionResult Add()
         {
             return View();
         }
+
+        public IActionResult Add(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.AddNewArticle(article);
+                return RedirectToAction("Dashboard");
+            }
+            return View(article);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var article = _service.GetArticles().FirstOrDefault(art => art.ID == id);
+
+            if(article == null)
+            {
+                return NotFound();
+            }
+            var model = new Article
+            {
+                ID = article.ID,
+                heading = article.heading,
+                date = article.date,
+                content = article.content
+            };
+            return View(model);
+        }
+
+        public IActionResult Edit(Article updatedArticle)
+        {
+            if (ModelState.IsValid)
+            {
+                bool updated = _service.EditArticleByID(
+                    updatedArticle.ID,
+                    updatedArticle.heading,
+                    updatedArticle.date,
+                    updatedArticle.content
+                    );
+
+                if (updated)
+                {
+                    return RedirectToAction("Dashboard");
+                }
+               
+            }
+            return View(updatedArticle);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            bool deleted = _service.DeleteArticleByID(id);
+            return RedirectToAction("Dashboard");
+        }
+        
     }
 }
